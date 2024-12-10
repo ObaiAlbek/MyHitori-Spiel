0fadf797f0b33d930dbbc2f00a3a9146111cb976
Florian Hörner <3012330@stud.hs-mannheim.de>
2024-12-10 15:13:36 +0100

    angepasst

--- src/main/java/de/hs_mannheim/informatik/hitori/main/Main.java
+++ src/main/java/de/hs_mannheim/informatik/hitori/main/Main.java
@@ -1,11 +1,14 @@
 package de.hs_mannheim.informatik.hitori.main;
 
 import de.hs_mannheim.informatik.hitori.gui.HitoriGame;
+import de.hs_mannheim.informatik.hitori.gui.Menu;
+import de.hs_mannheim.informatik.hitori.domain.*;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		new HitoriGame(6,6);
+		new HitoriGame(CsvEinlesen.getSpielfeld(0));
+		new Menu();
 	}
 
 }
